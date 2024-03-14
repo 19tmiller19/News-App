fdd195eb084d5a2b6346c374ecd195eb8489ac7c
Troy <98496851+19tmiller19@users.noreply.github.com>
2024-03-14 12:11:06 -0400

    Newsboard.jsx

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -7,7 +7,7 @@ const Newsboard = ({category}) => {
     const [articles, setArticles] = useState([]);
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/everything?q=keyword&apiKey=6aaae871558643f9ab6bc16736edeed9`;
+        let url = `https://newsapi.org/v2/everything?q=keyword&apiKey=${API_KEY}`;
         fetch(url).then(response=>response.json()).then(data=> setArticles(data.articles));
     },[])
 
